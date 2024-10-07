59cbbbdeafeeac7adea77b5d5d977c6faef19e73
Alexis-chata <alexizz.19.ac@gmail.com>
2024-10-07 15:02:33 -0500

    Mejora login

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/tailadmin.css',
+                'resources/js/tailadmin.js',
             ],
             refresh: true,
         }),
