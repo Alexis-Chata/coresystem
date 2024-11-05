50bb76318244d0192d41b90991af4ccef4c512d3
Cristian <criztian31@hotmail.com>
2024-11-05 14:56:04 -0500

    Vista Cliente + Padron

--- resources/views/cliente.blade.php
+++ resources/views/cliente.blade.php
@@ -4,9 +4,41 @@
 @section('content')
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <livewire:cliente-table/>
+    <br>
+    <livewire:padron-table/>
 <script>
     document.addEventListener('livewire:initialized', () => {
         Livewire.on('cliente-created', message => {
+            Swal.fire({
+                title: '¡Éxito!',
+                text: message,
+                icon: 'success',
+                confirmButtonText: 'OK'
+            });
+        });
+    });
+</script>
+<script>
+    document.addEventListener('livewire:initialized', () => {
+        Livewire.on('padron-deleted', message => {
+            Swal.fire({
+                title: '¡Éxito!',
+                text: message,
+                icon: 'success',
+                confirmButtonText: 'OK'
+            });
+        });
+
+        Livewire.on('padron-restored', message => {
+            Swal.fire({
+                title: '¡Éxito!',
+                text: message,
+                icon: 'success',
+                confirmButtonText: 'OK'
+            });
+        });
+
+        Livewire.on('padron-force-deleted', message => {
             Swal.fire({
                 title: '¡Éxito!',
                 text: message,
