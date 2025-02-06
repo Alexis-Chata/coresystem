44d2d74069da447b885ba024710dc38f9b2aef7c
Alexis-Chata <alexizz.19.ac@gmail.com>
2025-02-06 08:07:09 -0500

    git_pull deploy

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('git_pull', function(){
-    return Http::get('http://128.140.10.123:3000/api/box/deploy/4c6ee4554ce1435bf1d457abb315637efcf43314be732d0d');
-});
+// Route::post('git_pull', function(){
+//     return Http::get('http://128.140.10.123:3000/api/box/deploy/4c6ee4554ce1435bf1d457abb315637efcf43314be732d0d');
+// });
