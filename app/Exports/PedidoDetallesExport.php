a308eda02b85b6ce33abf39b4761b8acffa9af35
Alexis-chata <alexizz.19.ac@gmail.com>
2025-02-15 19:50:57 -0500

    Agregar comparación de precios y actualizar encabezados en la exportación de detalles de pedido

--- app/Exports/PedidoDetallesExport.php
+++ app/Exports/PedidoDetallesExport.php
@@ -2,7 +2,6 @@
 
 namespace App\Exports;
 
-use App\Models\PedidoDetalle;
 use App\Models\ProductoListaPrecio;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
@@ -33,20 +32,25 @@ class PedidoDetallesExport implements FromCollection, WithHeadings, ShouldAutoSi
             ->join('marcas', 'productos.marca_id', '=', 'marcas.id')
             ->join('pedido_detalles', 'pedido_detalles.producto_id', '=', 'productos.id')
             ->join('pedidos', 'pedido_detalles.pedido_id', '=', 'pedidos.id')
+            ->join('producto_lista_precios', function ($join) {
+                $join->on('producto_lista_precios.producto_id', '=', 'productos.id')
+                    ->on('producto_lista_precios.lista_precio_id', '=', 'pedido_detalles.lista_precio');
+            })
             ->select([
                 'pedidos.cliente_id as cliente_cod',
                 'pedidos.fecha_emision as pedido_fecha',
                 'marcas.name as marca_name',
                 'pedido_detalles.*',
+                'producto_lista_precios.precio as precio_actual', // Agregamos el precio actual
             ])
             ->selectRaw('FLOOR(pedido_detalles.cantidad) as bultos')
             ->selectRaw('CAST((pedido_detalles.cantidad - FLOOR(pedido_detalles.cantidad)) * 100 AS UNSIGNED) as unidades')
             ->selectRaw('CAST(ROUND( pedido_detalles.producto_precio * FLOOR(pedido_detalles.cantidad) + ((pedido_detalles.producto_precio * (pedido_detalles.cantidad - FLOOR(pedido_detalles.cantidad)) * 100) / pedido_detalles.producto_cantidad_caja) + 0.0001, 2) AS DECIMAL(10,2)) as importe2')
             ->selectRaw('CASE WHEN CAST(ROUND( pedido_detalles.producto_precio * FLOOR(pedido_detalles.cantidad) + ((pedido_detalles.producto_precio * (pedido_detalles.cantidad - FLOOR(pedido_detalles.cantidad)) * 100) / pedido_detalles.producto_cantidad_caja) + 0.0001, 2) AS DECIMAL(10,2)) = pedido_detalles.importe THEN 1 ELSE 0 END as importe_igual')
+            ->selectRaw('CASE WHEN pedido_detalles.producto_precio = producto_lista_precios.precio THEN 1 ELSE 0 END as precio_igual') // Nueva comparación de precios
             ->whereBetween('pedidos.fecha_emision', [$fechaInicio, $fechaFin])
             ->orderBy('pedido_detalles.id', 'asc')
             ->get();
-
 
         return $pedidoDetalles;
     }
@@ -70,10 +74,12 @@ class PedidoDetallesExport implements FromCollection, WithHeadings, ShouldAutoSi
                 'importe',
                 'created_at',
                 'updated_at',
+                'precio_actual',
                 'bultos',
                 'unidades',
                 'importe2',
-                'verificacion',
+                'verificacion_importe',
+                'verificacion_precio',
             ],
         ];
 
