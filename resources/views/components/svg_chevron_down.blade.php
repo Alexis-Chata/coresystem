6a3eb1bdd14d86d56fd016df506bb64196381bc6
Alexis-chata <alexizz.19.ac@gmail.com>
2026-02-09 01:53:51 -0500

    Añadir estilos y nuevos componentes SVG

---
+++ resources/views/components/svg_chevron_down.blade.php
@@ -0,0 +1,6 @@
+<svg class="-translate-y-1/2 absolute dark:text-gray-500 h-5 pointer-events-none right-3 text-gray-400 top-1/2 w-5"
+    fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+    <path clip-rule="evenodd"
+        d="M5.23 7.21a.75.75 0 011.06-.02L10 10.585l3.71-3.395a.75.75 0 011.04 1.08l-4 3.75a.75.75 0 01-1.04 0l-4-3.75a.75.75 0 01-.02-1.06z"
+        fill-rule="evenodd" />
+</svg>
