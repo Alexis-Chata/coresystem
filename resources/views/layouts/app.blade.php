4199cc9418937d1d7c40cbf6142f44226fb3c09b
Alexis-chata <alexizz.19.ac@gmail.com>
2026-02-09 20:19:14 -0500

    Añadir transiciones al preloader en la vista

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,7 +101,8 @@
         <x-banner />
 
         <!-- ===== Preloader Start ===== -->
-        <div x-show="loaded" x-init="window.addEventListener('DOMContentLoaded', () => { setTimeout(() => loaded = false, 500) })"
+        <div x-show="loaded" x-init="window.addEventListener('DOMContentLoaded', () => { setTimeout(() => loaded = false, 500) })" x-transition:leave="transition ease-in duration-300"
+            x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
             class="fixed left-0 top-0 z-999999 flex h-screen w-screen items-center justify-center bg-white dark:bg-black">
             <div class="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent">
             </div>
